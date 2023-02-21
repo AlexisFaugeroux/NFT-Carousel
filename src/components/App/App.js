6c3b0fe606f918bba04150b60bd6db2cc751bf96
AlexisFaugeroux <faugeroux.alexis@gmail.com>
2023-02-21 11:32:51 +0100

    Remove unecessary files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,6 @@ import './style.scss';
 import { Route, Routes } from 'react-router-dom';
 
 /* Components */
-// import Header from '../Header/Header';
 import Landing from '../Landing/Landing';
 
 function App() {
@@ -17,35 +16,12 @@ function App() {
         {/* HOME */}
         <Route path='/' element={
           <>
-            {/* <Header /> */}
             <Landing />
-            {/* <Footer /> */}
           </>}
         />
 
         {/* ARTWORK DETAIL PAGE */}
         {/* <Route path='/artwork/:id' element={<Artwork />} /> */}
-
-        {/* ARTISTS */}
-        {/* <Route path='/artists' element={<Artists />} /> */}
-
-        {/* COLLECTIONS */}
-        {/* <Route path='/collections' element={<Collections />} /> */}
-
-        {/* CONVERTOR */}
-        {/* <Route path='/convertor' element={<Convertor />} /> */}
-
-        {/* COLLECTION */}
-        {/* <Route path='/collection' element={<Collection />} /> */}
-
-        {/* ROADMAP */}
-        {/* <Route path='/roadmap' element={<RoadMap />} /> */}
-
-        {/* CHARITY */}
-        {/* <Route path='/charity' element={<Charity />} /> */}
-
-        {/* SEARCH */}
-        {/* <Route path='/search' element={<Search />} /> */}
 
         {/* 404 */}
         <Route path='*' element=
@@ -57,22 +33,6 @@ function App() {
             </>
           }
         />
-
-        {/* PROFILE */}
-        {/* <Route path='/profile' element={<Profile />} /> */}
-
-        {/* SIGN UP */}
-        {/* <Route path='/signup' element={<SignUp />} /> */}
-
-        {/* SIGN IN */}
-        {/* <Route path='/signin' element={<SignIn />} /> */}
-
-        {/* CONTACT */}
-        {/* <Route path='/contact' element={<Contact />} /> */}
-
-        {/* Admin */}
-        {/* <Route path='/login-team' element={<Admin />} /> */}
-        {/* <Route path='/admin/dashboard' element={<Protected />} /> */}
 
       </Routes>
     </div>
--- src/utils/fetchUserInfos.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const fetchUserInfos = async (id) => {
-    const response = await axios.get(`http://localhost:8000/users/${id}`);
-    console.log(response);
-    return response.data;
-}
